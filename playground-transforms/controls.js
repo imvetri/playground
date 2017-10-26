8709414e2ca079c6b842e1cc831e1437c2c13bbf
imvetri <svetrivel.91@gmail.com>
2017-10-26 19:30:31 +0530

    adds scaling feature

--- playground-transforms/controls.js
+++ playground-transforms/controls.js
@@ -1,8 +1,19 @@
 class Controls {
     constructor(config) {
+        this.queryElements(config);
+        this.bindEvents();
+    }
 
+    bindEvents() {
+        this.xScale.addEventListener("change", e => this.item.style.transform = "scaleX("+e.target.value +")");
+        this.yScale.addEventListener("change", e => this.item.style.transform = "scaleY("+e.target.value +")");
+    }
+    queryElements(config) {
+        Object.keys(config)
+            .forEach(key => this[key] = document.querySelector(config[key]));
     }
 }
+
 
 new Controls({
     item: '#item',
